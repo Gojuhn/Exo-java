29751b02711da1fdd85f1d9bd7f71cf23e1cda9c
johono <johnstarjuhn.jg@gmail.com>
2023-01-03 18:59:03 +0400

    Pokerito-file

---
+++ Main.java
@@ -0,0 +1,188 @@
+public class Main {
+    public static void main(String[] args) {
+
+        printCard(randomCard());
+
+    }
+
+
+    /**
+     *
+     * @return
+     *
+     * This function return a random number between From 1 to 5
+     */
+    public static int randomCard(){
+        double number = (Math.random() * 12) + 1 ;
+
+
+        return (int) number;
+
+
+
+    }
+
+    /**
+     *
+     * @param numberCard
+     *
+     * This function print Cards
+     */
+    public static void printCard(int numberCard){
+
+        switch (numberCard){
+
+            case 1 :
+
+                System.out.println(
+                        "   _____\n"+
+                        "  |A _  |\n"+
+                        "  | ( ) |\n"+
+                        "  |(_'_)|\n"+
+                        "  |  |  |\n"+
+                        "  |____V|\n");
+                break;
+            case 2:
+
+                System.out.println(
+                        "   _____\n"+
+                        "  |2    |\n"+
+                        "  |  o  |\n"+
+                        "  |     |\n"+
+                        "  |  o  |\n"+
+                        "  |____Z|\n"
+                );
+                break;
+
+            case 3:
+                System.out.println(
+                        "   _____\n" +
+                        "  |3    |\n"+
+                        "  | o o |\n"+
+                        "  |     |\n"+
+                        "  |  o  |\n"+
+                        "  |____E|\n"
+                );
+                break;
+
+            case 4:
+                System.out.println(
+                        "   _____\n" +
+                        "  |4    |\n"+
+                        "  | o o |\n"+
+                        "  |     |\n"+
+                        "  | o o |\n"+
+                        "  |____h|\n"
+                );
+                break;
+            case 5:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |5    |\n" +
+                        "  | o o |\n" +
+                        "  |  o  |\n" +
+                        "  | o o |\n" +
+                        "  |____S|\n"
+                );
+                break;
+
+
+            case 6:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |6    |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  |____6|\n"
+
+                );
+                break;
+            case 7:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |7    |\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |____7|\n"
+
+                );
+                break;
+            case 8:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |8    |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  |____8|\n"
+
+                );
+                break;
+            case 9:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |9    |\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |____9|\n"
+
+                );
+                break;
+            case 10:
+                System.out.println(
+                        "   _____ \n" +
+                        "  |10  o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |___10|\n"
+
+                );
+                break;
+            case 11:
+                System.out.println(
+                        "   _____\n" +
+                        "  |J  ww|\n"+
+                        "  | o {)|\n"+
+                        "  |o o% |\n"+
+                        "  | | % |\n"+
+                        "  |__%%[|\n"
+
+                );
+                break;
+            case 12:
+                System.out.println(
+                        "   _____\n" +
+                        "  |Q  ww|\n"+
+                        "  | o {(|\n"+
+                        "  |o o%%|\n"+
+                        "  | |%%%|\n"+
+                        "  |_%%%O|\n"
+
+                );
+                break;
+            case 13:
+                System.out.println(
+                        "   _____\n" +
+                        "  |K  WW|\n"+
+                        "  | o {)|\n"+
+                        "  |o o%%|\n"+
+                        "  | |%%%|\n"+
+                        "  |_%%%>|\n"
+
+                );
+                break;
+
+
+            default:
+                System.out.println("Error");
+
+        }
+
+    }
+
+
+}
