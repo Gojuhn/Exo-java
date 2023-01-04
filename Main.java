69a2766960e9b03b634d77ff7e7883a8045220c4
johono <johnstarjuhn.jg@gmail.com>
2023-01-04 05:53:36 +0400

    print card player1 and player2

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Here is your card : ");
+        int player1 = randomCard();
+        printCard(player1);
 
-        printCard(randomCard());
+        System.out.println("Here is the computer  card : ");
+        int player2 = randomCard();
+        while (player1 == player2){
+            player2 = randomCard();
+        }
+
+        printCard(player2);
 
     }
 
